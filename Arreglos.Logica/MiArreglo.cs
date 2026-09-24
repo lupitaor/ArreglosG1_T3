bf34080b5824608f45c3506a60793deea5ffed8f
lupitaor <amartinez@itesa.edu.mx>
2026-09-24 14:03:09 -0600

    Versión_03: Método ordenar ascendente descendente.

--- Arreglos.Logica/MiArreglo.cs
+++ Arreglos.Logica/MiArreglo.cs
@@ -35,6 +35,52 @@
             _tope = N;
         }
 
+
+        //Método ordenar ( Burbuja)
+
+        public void Ordenar()
+        {
+            Ordenar(true);
+        }
+
+        public void Ordenar(bool ascendente)
+        {
+            for (int i = 0; i < _tope - 1; i++)
+            {
+                for (int j = i + 1; j < _tope; j++)
+                {
+                    if (ascendente)
+                    {
+                        if (_arreglo[i] > _arreglo[j])
+                        {
+                            Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        }
+                    }
+                    else
+                    {
+                        if (_arreglo[i] < _arreglo[j])
+                        {
+                            Cambiar(ref _arreglo[i], ref _arreglo[j]);
+                        }
+                    }
+
+
+
+
+
+                }
+            }
+        }
+        //Métod Cambiar
+        public void Cambiar(ref int a, ref int b)
+        {
+            int aux = a;
+            a = b;
+            b = aux;
+        }
+
+
+
         //Método ToString
         public override string ToString()
         {
--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -2,11 +2,22 @@
 
 Console.WriteLine("Operaciones de pila");
 
-Console.WriteLine("Arreglo");
-MiArreglo oMiArreglo = new MiArreglo(100);
+Console.WriteLine("Arreglo\n");
+MiArreglo oMiArreglo = new MiArreglo(10);
 oMiArreglo.Llenar(1, 20);
 
+Console.WriteLine("Arreglo desordenado\n");
 Console.WriteLine(oMiArreglo);
+
+Console.WriteLine("Arreglo ordenado ascendente\n");
+oMiArreglo.Ordenar();
+Console.WriteLine(oMiArreglo);
+
+Console.WriteLine("Arreglo ordenado descendente\n");
+oMiArreglo.Ordenar(false);
+Console.WriteLine(oMiArreglo);
+
+
 
 
 Console.ReadKey();
